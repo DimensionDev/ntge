40c8992b62a4e5ebe3db7e119745e5789ce0b697
Tlaster <tlaster@outlook.com>
2020-04-17 17:40:06 +0800

    Add ntge-android

---
+++ ntge-android/src/test/java/com/dimension/ntge/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.dimension.ntge
+
+import org.junit.Test
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    fun test() {
+    }
+
+}
+
+
